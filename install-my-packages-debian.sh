8a0de4729f629580afb266e2592a6838623eb591
Brian VanderZanden <brian@agentc.io>
2016-03-10 14:25:35 -0800

    Adds support for Debian.
    Somewhat specific for RaspberryPi.

---
+++ install-my-packages-debian.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -x
+
+#Update package listings
+sudo apt-get update
+
+# Install fail2ban
+sudo apt-get install fail2ban -y
+
+# Install zsh
+sudo apt-get install zsh -y
+
+# Install htop
+sudo apt-get install htop -y
+
+# Install Mosh
+sudo apt-get install mosh -y
+
+# Install Multitail
+sudo apt-get install multitail -y
+
+# Install automatic upgrades
+# Configuration available in: /etc/apt/apt.conf.d/50unattended-upgrades
+sudo apt-get install unattended-upgrades bsd-mailx -y
+#Create cron job for the package.
+sudo cp resources/02periodic /etc/apt/apt.conf.d/02periodic
+
+echo "Done."
